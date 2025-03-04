3d38cd2e98d153ce9b37695f95c40739304e4969
Sebastian Saldivar <sebastiansaldivar092@gmail.com>
2025-03-04 02:59:00 -0300

    asdasd

--- vistas/eventos.php
+++ vistas/eventos.php
@@ -188,7 +188,7 @@ if (!isset($_SESSION['user_id'])) {
 </footer>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="../scripts/buscarci.js"></script>
+    <script src="scripts/buscarci.js"></script>
 
 
     <script>
